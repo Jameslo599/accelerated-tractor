c72e0a102d9a93380923d4ae87b0b56c69d623ff
James Lo <73731156+Jameslo599@users.noreply.github.com>
2025-09-24 10:59:27 -0500

    finished links

--- src/views/landings/default/data/hero.jsx
+++ src/views/landings/default/data/hero.jsx
@@ -2,7 +2,7 @@ export const hero = {
   chip: {},
   headLine: 'ACCELERATED TRACTOR SERVICES',
   captionLine: 'SERVING ALL OF NORTH TEXAS',
-  primaryBtn: { children: 'GET A QUOTE', href: '#' },
+  primaryBtn: { children: 'GET A QUOTE', href: '/contact' },
   videoSrc: 'https://player.vimeo.com/video/1119673776?autoplay=1&muted=1&loop=1&autopause=0&background=1',
   videoThumbnail: '/assets/videos/thumbnails/thumb.jpg',
   listData: []
--- src/views/landings/default/data/other.js
+++ src/views/landings/default/data/other.js
@@ -3,78 +3,79 @@ const imagePrefix = '/assets/images/presentation';
 
 // @project
 import branding from '@/branding.json';
+import { SECTION_PATH } from '@/path';
 
 export const other = {
   heading: `OUR SERVICES`,
   description:
     'Our expert land clearing contractors use state-of-the-art equipment to prepare your land for development, agriculture, or any project you have in mind. We deliver fast, efficient, and reliable results — giving you a clean slate to build your vision.',
-  primaryBtn: { children: 'CALL US NOW', href: '#' },
+  primaryBtn: { children: 'CALL US NOW', href: 'tel:+2145325700' },
   sections: [
     {
       animationDelay: 0.2,
       title: 'Driveways',
       image: { light: `${imagePrefix}/driveways2.webp`, dark: `${imagePrefix}/driveways2.webp` },
-      link: '#'
+      link: `${SECTION_PATH}/driveways`
     },
     {
       animationDelay: 0.3,
       title: 'Grading',
       image: { light: `${imagePrefix}/grading2.webp`, dark: `${imagePrefix}/grading2.webp` },
-      link: '#'
+      link: `${SECTION_PATH}/grading`
     },
     {
       animationDelay: 0.4,
       title: 'Clearing',
       image: { light: `${imagePrefix}/clearing2.webp`, dark: `${imagePrefix}/clearing2.webp` },
-      link: '#'
+      link: `${SECTION_PATH}/clearing`
     },
     {
       animationDelay: 0.2,
       title: 'Mowing',
       image: { light: `${imagePrefix}/mowing2.webp`, dark: `${imagePrefix}/mowing2.webp` },
-      link: '#'
+      link: `${SECTION_PATH}/mowing`
     },
     {
       animationDelay: 0.3,
       title: 'Building Pads',
       image: { light: `${imagePrefix}/building-pads2.webp`, dark: `${imagePrefix}/building-pads2.webp` },
-      link: '#'
+      link: `${SECTION_PATH}/building-pads`
     },
     {
       animationDelay: 0.4,
       title: 'Arenas',
       image: { light: `${imagePrefix}/arena.webp`, dark: `${imagePrefix}/arena.webp` },
-      link: '#'
+      link: `${SECTION_PATH}/arenas`
     },
     {
       animationDelay: 0.4,
       title: 'Excavation',
       image: { light: `${imagePrefix}/excavation2.webp`, dark: `${imagePrefix}/excavation2.webp` },
-      link: '#'
+      link: `${SECTION_PATH}/excavation`
     },
     {
       animationDelay: 0.4,
       title: 'Demolition',
       image: { light: `${imagePrefix}/demolition2.webp`, dark: `${imagePrefix}/demolition2.webp` },
-      link: '#'
+      link: `${SECTION_PATH}/demolition`
     },
     {
       animationDelay: 0.4,
       title: 'Leveling',
       image: { light: `${imagePrefix}/leveling2.webp`, dark: `${imagePrefix}/leveling2.webp` },
-      link: '#'
+      link: `${SECTION_PATH}/leveling`
     },
     {
       animationDelay: 0.4,
       title: 'Material Removal',
       image: { light: `${imagePrefix}/material-removal2.webp`, dark: `${imagePrefix}/material-removal2.webp` },
-      link: '#'
+      link: `${SECTION_PATH}/material-removal`
     },
     {
       animationDelay: 0.4,
       title: 'Fence Line Clearing',
       image: { light: `${imagePrefix}/fence-line-clearing.webp`, dark: `${imagePrefix}/fence-line-clearing.webp` },
-      link: '#'
+      link: `${SECTION_PATH}/fence-line-clearing`
     }
   ]
 };
