aa46523aae99368968128b874a09f649e8dbc559
James Lo <73731156+Jameslo599@users.noreply.github.com>
2025-10-14 23:53:25 -0500

    removed meta trailing slash

--- src/utils/Loader.js
+++ src/utils/Loader.js
@@ -9,8 +9,8 @@ export default function Loader() {
   return `<!doctype html>
 <html lang="en">
   <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="UTF-8" >
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" >
     <title>Linear Progress Loader</title>
     <style>
     body {
